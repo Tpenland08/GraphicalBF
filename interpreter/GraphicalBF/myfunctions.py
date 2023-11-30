210d6845fd8f95b830863c52d1c4bca6a9d02dd2
Tpenland08 <77027609+Tpenland08@users.noreply.github.com>
2023-11-30 14:07:00 +0000

    Added basic BF interpreter capabilities, added a log file, and added many comments for readability

--- interpreter/GraphicalBF/myfunctions.py
+++ interpreter/GraphicalBF/myfunctions.py
@@ -1,29 +1,54 @@
 
 import logging
 
+#Initialize log directory
+logging.basicConfig(filename="interpreter/logs/logs.txt")
 
+# The function to execute GraphicalBF programs
 def gbfExec(prgm: str, input: str):
+  # Set up variables pre-execution
   stack = [0] * 30000
   stackPtr = 0
-  for prgmPtr in range(len(prgm)):
+  prgmPtr = 0
+  # Print the program to let the user know what is running
+  print(prgm)
+  # Loop to read each character and execute each one
+  #appropriatelly
+  while prgmPtr < len(prgm):
     match prgm[prgmPtr]:
+      # Move the pointer one cell to the right
       case ">":
          stackPtr += 1
+      # Move the pointer one cell to the left
       case "<":
          stackPtr -= 1
+      # Increment the cell value at the pointer by 1
       case "+":
          stack[stackPtr] += 1
+      # Decrement the cell value at the pointer by 1
       case "-":
         stack[stackPtr] -= 1
+      # Start a loop by checking if the current cell
+      #value is 0, and if it is, skipping the loop
       case "[":
         if stack[stackPtr] == 0:
           prgmPtr = prgm.index("]", prgmPtr)
+          prgmPtr += 1
+      # Ending a loop by checking for a 0 value in the
+      # current cell, and if it isn't, going back to the
+      # start of the loop
       case "]":
-         prgmPtr = prgm.index("[", 0)
+         if stack[stackPtr] != 0:
+           prgmPtr = prgm.index("[", 0)
+      # Print the value at the current cell
+      # (Eventually use C type printing for alphanumeric
+      # vs number printing? Also print to screen as
+      # turtle text?)
       case ".":
          print(stack[stackPtr])
       case ",":
          pass
+      # Draw a frame to the screen (See the gbfPush func.)
       case "*":
          gbfPush()
       case _:
@@ -31,12 +56,24 @@ def gbfExec(prgm: str, input: str):
     prgmPtr += 1
   return(0)
 
+# Useless function, disregard
 def gbfDegraphic(prgm):
   return("Hello World")
 
-def gbfPush():
-  return("Hello World")
 
+# Function to draw a frame to the screen based on the contents of various stack cells
+def gbfPush():
+#  for pixel in range(69):
+#    color = bin(pixel)
+#    rgbValues = {
+#      "red": int(),
+#      "green": int(),
+#      "blue": int(slice(color, 2, 4))
+#    }
+    # turtle.color()  
+  return(0)
+
+# Don't remember what this function does
 def gbfCheckInput():
   return("Hello World")
 
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 """
 https://docs.python.org/3/library/turtle.html
 """
+
+# Import libraries turtle for drawing and sys for sys.path.append
 import turtle
 import sys
 
+# Change the path to look for modules and import the library
 sys.path.append('./interpreter')
 from GraphicalBF import myfunctions as gbf
 
@@ -14,8 +17,12 @@ screen.setup(1.0, 1.0)
 # Begin!
 t = turtle.Turtle()
 
-gbf.gbfExec("+++[>+++<-].*", "41")
+# Call gbfExec from the gbf module to execute the program
+# with argument program, input as a string, each
+# character is one input
+gbf.gbfExec("+++[>+++++<-]>.*", "4")
 
+# Not important code
 for c in ['red', 'green', 'blue', 'yellow']:
   t.color(c)
   t.forward(75)
